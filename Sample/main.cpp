62e602b42649246c802d859db52f071e20c8ee20
Alexis <alexisvt@gmail.com>
2017-04-26 15:36:15 -0600

    adding configuration files for vscode

---
+++ Sample/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  int age = 0;
+  cout << "Type your age: \n";
+  cin >> age;
+  cout << "Your age is " << age << endl;
+
+  return 0;
+}
--- VSSample/VSSample/VSSample.cpp
+++ VSSample/VSSample/VSSample.cpp
@@ -10,6 +10,17 @@ using namespace std;
 int main()
 {
 	// insertion operator
-	cout << "Hola mundo" << endl << 2 + 2;
+	cout << "Hola mundo" << endl << 2 + 2 << endl;
+
+	int i = 4.3;
+
+	cout << i << endl;
+
+	// to produce a floating point result we need to make sure that
+	// one of the digit is a point number
+	float j = 9.0 / 5;
+
+	cout << j << endl;
+
 	return 0;
 }
